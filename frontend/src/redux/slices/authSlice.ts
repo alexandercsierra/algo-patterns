72fa28e7289db4caf2bcb818177ddccfa3eba6fe
alexandercsierra <alexandercsierra@gmail.com>
2025-03-31 11:52:38 -0400

    fix types in authslice

--- frontend/src/redux/slices/authSlice.ts
+++ frontend/src/redux/slices/authSlice.ts
@@ -1,10 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface AuthState {
+  user: null | { _id: string; name: string; email: string; photo: string };
+}
+
+const initialState: AuthState = {
+  user: null,
+};
+
 export const authSlice = createSlice({
   name: "auth",
-  initialState: {
-    user: null,
-  },
+  initialState,
   reducers: {
     addUser: (state, action) => {
       if (action.payload?._id !== state.user?._id) {
